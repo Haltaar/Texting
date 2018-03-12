5324e940b05a44e1cd39efa0884785fe3be327ad
haltaar <sethjwilkin@gmail.com>
2018-03-12 17:07:38 +0000

    added dialog to set shift value for caeser cipher. TODO implement further ciphers

--- app/src/main/java/com/example/haltaar/texting/ThreadActivity.java
+++ app/src/main/java/com/example/haltaar/texting/ThreadActivity.java
@@ -1,6 +1,7 @@
 package com.example.haltaar.texting;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.ContentResolver;
@@ -20,8 +21,10 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.AdapterView.AdapterContextMenuInfo;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.NumberPicker;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -79,10 +82,42 @@ public class ThreadActivity extends AppCompatActivity {
 
     @Override
     public boolean onContextItemSelected(MenuItem item) {
-        if (item.getItemId() == 2131165243){ //checking that item id is correct for the message list
-            AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo)item.getMenuInfo();
-            smsMessagesList.set(info.position, ciphers.caesar(smsMessagesList.get(info.position), 32));
-            arrayAdapter.notifyDataSetChanged();
+        Log.d(TAG, "onContextItemSelected: init");
+        Log.d(TAG, "onContextItemSelected: item id" + item.getItemId());
+        if (item.getItemId() == 2131165245 ){ //checking that item id is correct for the message list 2131165243
+            Log.d(TAG, "onContextItemSelected: if statement satisfied");
+            final AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo)item.getMenuInfo();
+
+            AlertDialog.Builder alertBuilder = new AlertDialog.Builder(this);
+            View alertView = getLayoutInflater().inflate(R.layout.dialog_caeser, null);
+
+            final NumberPicker alertNumberPicker = (NumberPicker) alertView.findViewById(R.id.numberPicker);
+            alertNumberPicker.setMinValue(1);
+            alertNumberPicker.setMaxValue(26);
+            alertNumberPicker.setValue(13);
+            Button alertButtonOkay = (Button) alertView.findViewById(R.id.buttonOkay);
+            Button alertButtonCancel = (Button) alertView.findViewById(R.id.buttonCancel);
+
+            alertBuilder.setView(alertView);
+            final AlertDialog dialog = alertBuilder.create();
+
+            alertButtonOkay.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    int shift = (int) alertNumberPicker.getValue();
+                    smsMessagesList.set(info.position, ciphers.caesar(smsMessagesList.get(info.position), shift));
+                    arrayAdapter.notifyDataSetChanged();
+                    dialog.dismiss();
+                }
+            });
+
+            alertButtonCancel.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    dialog.dismiss();
+                }
+            });
+            dialog.show();
             return true;
         }
         else
